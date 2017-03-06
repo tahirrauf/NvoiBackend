fb4366c9be85e63bb8a9118ab0cf0c00f305dc9e
Tahir Rauf <tahir.abdul.rauf@gmail.com>
2017-03-06 12:13:11 +0000

    Added foundation API placeholders and added relevant nuget packages

--- Nvoi.Backen.API/App_Start/WebApiConfig.cs
+++ Nvoi.Backen.API/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Newtonsoft.Json.Serialization;
 
 namespace Nvoi.Backen.API
 {
@@ -14,11 +15,12 @@ namespace Nvoi.Backen.API
             // Web API routes
             config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+            config.EnableCors();
+            var json = config.Formatters.JsonFormatter;
+            json.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+            json.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
         }
     }
 }
---
+++ Nvoi.Backen.API/Controllers/MerchantController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Nvoi.Backen.API.Controllers
+{
+    [EnableCors(origins: "http://localhost:55000", headers: "*", methods: "*")]
+    public class MerchantController : ApiController
+    {
+        
+        [Route("api/merchants")]
+        public HttpResponseMessage GetAllMerchants()
+        {
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, "");
+            return response;
+        }
+
+        [Route("api/merchants/{id?}")]
+        public HttpResponseMessage GetMerchantbyId(int id)
+        {
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, "");
+            return response;
+        }
+
+        [Route("api/merchants/active")]
+        public HttpResponseMessage GetActiveMerchants()
+        {
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, "");
+            return response;
+        }
+
+        [Route("api/merchants")]
+        public HttpResponseMessage Post(Object merchantObject)
+        {
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, merchantObject);
+            return response;
+        }
+
+        [Route("api/merchants")]
+        public HttpResponseMessage Put(Object merchantObject)
+        {
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, merchantObject);
+            return response;
+        }
+
+        [Route("api/merchants")]
+        public HttpResponseMessage Delete(Object merchantObject)
+        {
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, merchantObject);
+            return response;
+        }
+    }
+   }
